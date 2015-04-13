695e98d36e496bd7db845baba32e206da205b010
jrf254 <jrf254@nau.edu>
2015-04-12 17:06:43 -0700

    Added beginning of enemy

---
+++ src/com/defense/haxe/enemy/Enemy.hx
@@ -0,0 +1,13 @@
+import com.cykon.haxe.movable.circle.Circle;
+import com.cykon.haxe.cmath.Vector;
+
+class Enemy extends Circle{
+	public function new(){
+		super();
+	}
+
+	public override function applyVelocity(modifier:Float):Bool{
+		super.applyVelocity(modifier);
+
+	}
+}
