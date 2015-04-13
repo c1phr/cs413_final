401a5e9baeb6b43a0a07b79cc3dda6d0552a3acd
jrf254 <jrf254@nau.edu>
2015-04-12 17:25:54 -0700

    More enemy

--- src/com/defense/haxe/enemy/Enemy.hx
+++ src/com/defense/haxe/enemy/Enemy.hx
@@ -1,13 +1,24 @@
 import com.cykon.haxe.movable.circle.Circle;
 import com.cykon.haxe.cmath.Vector;
+import com.cykon.haxe.movable.Point;
 
 class Enemy extends Circle{
+	private var pointArray:Array<Point>;
+
 	public function new(){
 		super();
 	}
 
 	public override function applyVelocity(modifier:Float):Bool{
 		super.applyVelocity(modifier);
+		var velocityVector = new Vector(vx, vy);
+		var pointVector = Vector.getVector(x,y,pointx,pointy);
+		if (velocityVector.dot(pointVector) < 0){
+			x = pointx;
+			y = pointy;
+
+			velocityVector.getMag()
+		}
 
 	}
 }
