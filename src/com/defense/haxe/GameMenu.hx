7bfeee05e1bc191a4989445f6605a404e60d2f07
Ryan Batchelder <rb688@nau.edu>
2015-05-01 10:28:58 -0700

    Added tower descriptions to the tutorial screen

--- src/com/defense/haxe/GameMenu.hx
+++ src/com/defense/haxe/GameMenu.hx
@@ -33,8 +33,7 @@ class GameMenu extends Sprite {
 		this.startButton.x = 325 - (this.startButton.width/2);
 		this.startButton.y = 100;
 		this.startButton.addEventListener(TouchEvent.TOUCH, startGame);
-		this.addChild(this.startButton);
-
+		this.addChild(this.startButton);		
 		this.tutorialButton = createButton("Tutorial");
 		this.tutorialButton.x = 325 - (this.tutorialButton.width/2);
 		this.tutorialButton.y = 250;
@@ -43,10 +42,10 @@ class GameMenu extends Sprite {
 	}
 
 	private function createBackground():Quad {
-		var background = new Quad(550, 400, 0x000000);
+		var background = new Quad(650, 400, 0x000000);
 		background.x = 0;
 		background.y = 0;
-		background.alpha = 0.5;
+		background.alpha = 0.8;
 		return background;
 	}	
 
@@ -61,12 +60,59 @@ class GameMenu extends Sprite {
 
 	private function createTutorialScene():Sprite {
 		var tutorialScene = new Sprite();
-		var tutorialText = new TextField(500, 400, "Place towers to defeat enemies\nClick the down arrow to end build phase and start play phase", "Arial", 18, 0x00CCFF);
+		var tutorialText = new TextField(500, 75, "Place towers to defeat enemies\nClick Go to end build phase and start play phase", "Arial", 18, 0x00CCFF);
 		tutorialScene.addChild(tutorialText);
 		tutorialText.x = 325 - (tutorialText.width/2);
+		tutorialText.y = 25;
+
+		var wallImg = new Image(Root.assets.getTexture("wall_button"));
+		wallImg.x = 100;
+		wallImg.y = 100;
+		var wallText = new TextField(400, 50, "Wall does no damage. Cost - 5", "Arial", 18, 0x00CCFF);
+		wallText.x = wallImg.x + 40;
+		wallText.y = wallImg.y;
+		tutorialScene.addChild(wallText);
+		tutorialScene.addChild(wallImg);
+		
+		var redTowerImg = new Image(Root.assets.getTexture("redtower"));
+		redTowerImg.x = 100;
+		redTowerImg.y = 150;
+		var redTowerText = new TextField(400, 50, "Heavy damage tower. Cost - 100", "Arial", 18, 0x00CCFF);
+		redTowerText.x = redTowerImg.x + 40;
+		redTowerText.y = redTowerImg.y;
+		tutorialScene.addChild(redTowerText);
+		tutorialScene.addChild(redTowerImg);
+
+		var greenTowerImg = new Image(Root.assets.getTexture("greentower"));
+		greenTowerImg.x = 100;
+		greenTowerImg.y = 200;
+		var greenTowerText = new TextField(400, 50, "Causes damage over time. Cost - 150", "Arial", 18, 0x00CCFF);
+		greenTowerText.x = greenTowerImg.x + 40;
+		greenTowerText.y = greenTowerImg.y;
+		tutorialScene.addChild(greenTowerText);
+		tutorialScene.addChild(greenTowerImg);
+
+		var blueTowerImg = new Image(Root.assets.getTexture("bluetower"));
+		blueTowerImg.x = 100;
+		blueTowerImg.y = 250;
+		var blueTowerText = new TextField(400, 50, "Slows enemies. Cost - 150", "Arial", 18, 0x00CCFF);
+		blueTowerText.x = blueTowerImg.x + 40;
+		blueTowerText.y = blueTowerImg.y;
+		tutorialScene.addChild(blueTowerText);
+		tutorialScene.addChild(blueTowerImg);
+
+		var yellowTowerImg = new Image(Root.assets.getTexture("purpletower"));
+		yellowTowerImg.x = 100;
+		yellowTowerImg.y = 300;
+		var yellowTowerText = new TextField(400, 50, "Fires quickly but with less damage. Cost - 100", "Arial", 18, 0x00CCFF);
+		yellowTowerText.x = yellowTowerImg.x + 40;
+		yellowTowerText.y = yellowTowerImg.y;
+		tutorialScene.addChild(yellowTowerText);
+		tutorialScene.addChild(yellowTowerImg);
+
 		backButton = createButton("Go Back");
 		backButton.x = 325 - (this.backButton.width/2);
-		backButton.y = 300;
+		backButton.y = 400;
 		backButton.addEventListener(TouchEvent.TOUCH, function(event:TouchEvent){
 			if (event.touches[0].phase == "began")
 			{
